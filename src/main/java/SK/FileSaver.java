5b680206e700e486f9abcd6e8aface8ee5afd3ad
Serhii <serg14581@gmail.com>
2019-04-01 21:47:21 +0300

    test

--- src/main/java/SK/FileSaver.java
+++ src/main/java/SK/FileSaver.java
@@ -1,17 +1,20 @@
 package SK;
-
+import TD.Animal;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public abstract class FileSaver {
     public static void saveSeller(Seller seller) throws IOException {
+        checkDirExists("Sellers");
         String filePath = "Sellers\\"+seller.getLogin()+".txt";
         File file = new File(filePath);
         if(!file.exists()){
-            byte [] inputBytes = seller.getLogin().getBytes();
+            byte [] inputBytes = seller.getPassword().getBytes();
             ByteBuffer buffer = ByteBuffer.wrap(inputBytes);
             FileOutputStream fos = new FileOutputStream(filePath);
             FileChannel fileChannel = fos.getChannel();
@@ -20,4 +23,16 @@ public abstract class FileSaver {
             fos.close();
         }
     }
+
+    public static void saveCheck(Seller seller, Animal animal){
+        checkDirExists("Bills");
+        String filePath = "Bills\\" + new SimpleDateFormat("dd.MM.yy_HH.mm.ss").format(Calendar.getInstance().getTime())+".txt";
+    }
+
+    private static void checkDirExists(String dirName){
+        File file = new File(dirName);
+        if(!file.exists())
+            file.mkdir();
+    }
+
 }
