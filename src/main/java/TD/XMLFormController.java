9ecf0ed224b6e840107d03e5491e7a8c57cb4ea0
TD-user <td87td87@gmail.com>
2019-04-01 21:38:45 +0300

    first form controller update1

--- src/main/java/TD/XMLFormController.java
+++ src/main/java/TD/XMLFormController.java
@@ -1,4 +1,30 @@
 package TD;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class XMLFormController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField loginText;
+
+    @FXML
+    private JFXButton buttonLogin;
+
+    @FXML
+    private JFXTextField passworText;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
 }
