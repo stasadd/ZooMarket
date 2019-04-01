3b015c2d013a2d8424ffabf129f2dbee2900fcc2
TD-user <td87td87@gmail.com>
2019-04-01 22:29:45 +0300

    create controller add seller

---
+++ src/main/java/TD/XMLFormControllerAddSeller.java
@@ -0,0 +1,30 @@
+package TD;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class XMLFormControllerAddSeller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField AddloginText;
+
+    @FXML
+    private JFXButton buttonAdd;
+
+    @FXML
+    private JFXTextField AddPassworText;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
