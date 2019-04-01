92cabbb961263504b3cb156b5eee0817971213ae
Serhii <serg14581@gmail.com>
2019-04-01 21:25:32 +0300

    test

--- src/main/java/SK/FileSaver.java
+++ src/main/java/SK/FileSaver.java
@@ -1,4 +1,24 @@
 package SK;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public abstract class FileSaver {
+    public static void saveSeller(Seller seller) throws IOException {
+        String filePath = "Sellers\\"+seller.getLogin()+".txt";
+        File file = new File(filePath);
+        if(!file.exists()){
+            byte [] inputBytes = seller.getLogin().getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(inputBytes);
+            FileOutputStream fos = new FileOutputStream(filePath);
+            FileChannel fileChannel = fos.getChannel();
+            fileChannel.write(buffer);
+            fileChannel.close();
+            fos.close();
+        }
+    }
 }
