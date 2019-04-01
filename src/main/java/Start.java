a814c827e27482bf66d0ca8cb6c8164b7cb8810d
stasadd <stas.add@gmail.com>
2019-04-01 20:27:54 +0300

    Initial commit

---
+++ src/main/java/Start.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/PanelSnouFXML.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(500);
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("krya");
+        launch(args);
+    }
+}
